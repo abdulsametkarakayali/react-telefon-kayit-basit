b54d59321fe73047638a696f5e4b3b76f386f7f5
abdulsametkarakayali <51421757+abdulsametkarakayali@users.noreply.github.com>
2020-09-27 00:50:39 +0300

    telefon rehner uygulaması react ders denemeleri

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import List  from './List'
 import Form from './Form'
 import PropTypes from 'prop-types';
+
 class Contacts extends Component {
     static propTypes ={
         contacts:PropTypes.array.isRequired,
